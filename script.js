5ef06de16c2e430c2fbacaecb9fdbc8dc84349bc
Andrefalt <70585168+Andrefalt@users.noreply.github.com>
2021-10-07 20:55:00 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('keydown', (tecla)=>{
+  var element = document.querySelector('#divid');
+  var hor = getComputedStyle(document.querySelector('#divid')).left;
+  var ver = getComputedStyle(document.querySelector('#divid')).bottom;
+  /*
+  The variable lue can be changed for example, to make a character's speed higher when he levels up.
+  A variável lue pode ser alterada para por exemplo, deixar um personagem mais rápido quando ele subir de nível.
+  */
+  var lue = 2;
+  switch(tecla.key){
+    case "a": {
+      element.style.left = `${Number(hor.split("px")[0])-lue}px`
+      break;
+    }
+    case "d": {
+      element.style.left = `${Number(hor.split("px")[0])+lue}px`;
+      break;
+    }
+        case "w": {
+      element.style.bottom = `${Number(ver.split("px")[0])+lue}px`
+      break;
+    }
+    case "s": {
+      element.style.bottom = `${Number(ver.split("px")[0])-lue}px`;
+      break;
+    }
+  }
+  }
+)
